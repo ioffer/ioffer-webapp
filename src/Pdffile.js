a753bf371925b798c62db15e1025b0dc49b25fdc
Moin105 <moinlatif105@gmail.com>
2022-02-28 17:50:11 +0500

    changed css

--- src/Pdffile.js
+++ src/Pdffile.js
@@ -1,6 +1,6 @@
-import React, { useState, useRef } from 'react';
-import { usePdf } from '@mikecousins/react-pdf';
-import Pdf from './assets/document/Pdf.pdf'
+import React, { useState, useRef } from "react";
+import { usePdf } from "@mikecousins/react-pdf";
+import Pdf from "./assets/document/Pdf.pdf";
 
 const Pdffile = () => {
   const [page, setPage] = useState(1);
@@ -13,32 +13,30 @@ const Pdffile = () => {
   });
 
   return (
-    <div >
-       <div>
-      <canvas ref={canvasRef} />
-      {Boolean(pdfDocument && pdfDocument.numPages) && (
-        <nav>
-          <ul className="pager">
-            <li className="previous">
-              <button disabled={page === 1} onClick={() => setPage(page - 1)}>
-                Previous
-              </button>
-            </li>
-            <li className="next">
-              <button
-                disabled={page === pdfDocument.numPages}
-                onClick={() => setPage(page + 1)}
-              >
-                Next
-              </button>
-            </li>
-          </ul>
-        </nav>
-  
-      )}
-</div>    
-
+    <div>
+      <div>
+        <canvas ref={canvasRef} />
+        {Boolean(pdfDocument && pdfDocument.numPages) && (
+          <nav>
+            <ul className="pager">
+              <li className="previous">
+                <button disabled={page === 1} onClick={() => setPage(page - 1)}>
+                  Previous
+                </button>
+              </li>
+              <li className="next">
+                <button
+                  disabled={page === pdfDocument.numPages}
+                  onClick={() => setPage(page + 1)}
+                >
+                  Next
+                </button>
+              </li>
+            </ul>
+          </nav>
+        )}
+      </div>
     </div>
   );
 };
-export default Pdffile
+export default Pdffile;
